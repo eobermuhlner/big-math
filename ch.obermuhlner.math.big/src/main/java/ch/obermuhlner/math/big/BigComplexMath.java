b64989205caaa6af74daa0652aca2f6e9cab065c
Eric Obermühlner <eobermuhlner@gmail.com>
2018-03-16 22:19:09 +0100

    increased precision in calculations

--- ch.obermuhlner.math.big/src/main/java/ch/obermuhlner/math/big/BigComplexMath.java
+++ ch.obermuhlner.math.big/src/main/java/ch/obermuhlner/math/big/BigComplexMath.java
@@ -34,56 +34,58 @@ public class BigComplexMath {
 
 		BigDecimal expRe = BigDecimalMath.exp(x.re, mc);
 		return BigComplex.valueOf(
-				expRe.multiply(BigDecimalMath.cos(x.im, mc), mathContext),
-				expRe.multiply(BigDecimalMath.sin(x.im, mc), mathContext));
+				expRe.multiply(BigDecimalMath.cos(x.im, mc), mc).round(mathContext),
+				expRe.multiply(BigDecimalMath.sin(x.im, mc), mc)).round(mathContext);
 	}
 	
 	public static BigComplex sin(BigComplex x, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 		
 		return BigComplex.valueOf(
-				BigDecimalMath.sin(x.re, mc).multiply(BigDecimalMath.cosh(x.im, mc), mathContext),
-				BigDecimalMath.cos(x.re, mc).multiply(BigDecimalMath.sinh(x.im, mc), mathContext));
+				BigDecimalMath.sin(x.re, mc).multiply(BigDecimalMath.cosh(x.im, mc), mc).round(mathContext),
+				BigDecimalMath.cos(x.re, mc).multiply(BigDecimalMath.sinh(x.im, mc), mc).round(mathContext));
 	}
 	
 	public static BigComplex cos(BigComplex x, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
 		return BigComplex.valueOf(
-				BigDecimalMath.cos(x.re, mc).multiply(BigDecimalMath.cosh(x.im, mc), mathContext),
-				BigDecimalMath.sin(x.re, mc).multiply(BigDecimalMath.sinh(x.im, mc), mathContext).negate());
+				BigDecimalMath.cos(x.re, mc).multiply(BigDecimalMath.cosh(x.im, mc), mc).round(mathContext),
+				BigDecimalMath.sin(x.re, mc).multiply(BigDecimalMath.sinh(x.im, mc), mc).negate().round(mathContext));
 	}
 	
 	// 
 	// http://scipp.ucsc.edu/~haber/archives/physics116A10/arc_10.pdf
 	
 	public static BigComplex tan(BigComplex x, MathContext mathContext) {
-		return sin(x, mathContext).divide(cos(x, mathContext), mathContext);
+		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
+
+		return sin(x, mc).divide(cos(x, mc), mc).round(mathContext);
 	}
 
 	public static BigComplex atan(BigComplex x, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return log(I.subtract(x, mc).divide(I.add(x, mc), mc), mc).divide(I, mc).divide(TWO, mathContext);
+		return log(I.subtract(x, mc).divide(I.add(x, mc), mc), mc).divide(I, mc).divide(TWO, mc).round(mathContext);
 	}
 	
 	public static BigComplex acot(BigComplex x, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return log(x.add(I, mc).divide(x.subtract(I, mc), mc), mc).divide(I, mc).divide(TWO, mathContext);
+		return log(x.add(I, mc).divide(x.subtract(I, mc), mc), mc).divide(I, mc).divide(TWO, mc).round(mathContext);
 	}
 	
 	// https://en.wikipedia.org/wiki/Inverse_trigonometric_functions#Extension_to_complex_plane
 	public static BigComplex asin(BigComplex x, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return I.negate().multiply(log(I.multiply(x, mc).add(sqrt(BigComplex.ONE.subtract(x.multiply(x, mc), mc), mc), mc), mc), mathContext);
+		return I.negate().multiply(log(I.multiply(x, mc).add(sqrt(BigComplex.ONE.subtract(x.multiply(x, mc), mc), mc), mc), mc), mc).round(mathContext);
 	}
 
 	public static BigComplex acos(BigComplex x, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return I.negate().multiply(log(x.add(sqrt(x.multiply(x, mc).subtract(BigComplex.ONE, mc), mc), mc), mc), mathContext);
+		return I.negate().multiply(log(x.add(sqrt(x.multiply(x, mc).subtract(BigComplex.ONE, mc), mc), mc), mc), mc).round(mathContext);
 	}
 	
 
@@ -94,25 +96,26 @@ public class BigComplexMath {
 		BigDecimal magnitude = x.abs(mc);
 
 		BigComplex a = x.add(magnitude, mc);
-		return a.divide(a.abs(mc), mc).multiply(BigDecimalMath.sqrt(magnitude, mc), mathContext);
+		return a.divide(a.abs(mc), mc).multiply(BigDecimalMath.sqrt(magnitude, mc), mc).round(mathContext);
 	}
 	
 	// https://en.wikipedia.org/wiki/Complex_logarithm
 	public static BigComplex log(BigComplex x, MathContext mathContext) {
-		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
+		MathContext mc1 = new MathContext(mathContext.getPrecision() + 20, mathContext.getRoundingMode());
+		MathContext mc2 = new MathContext(mathContext.getPrecision() + 5, mathContext.getRoundingMode());
 
 		return BigComplex.valueOf(
-				BigDecimalMath.log(x.abs(mc), mathContext),
-				toRangePi(x.angle(mathContext), mathContext));
+				BigDecimalMath.log(x.abs(mc1), mc1).round(mathContext),
+				toRangePi(x.angle(mc2), mc2)).round(mathContext);
 	}
 
 	public static BigComplex pow(BigComplex x, long y, MathContext mathContext) {
-		if (y < 0) {
-			return BigComplex.ONE.divide(pow(x, -y, mathContext), mathContext);
-		}
-		
 		MathContext mc = new MathContext(mathContext.getPrecision() + 10, mathContext.getRoundingMode());
 
+		if (y < 0) {
+			return BigComplex.ONE.divide(pow(x, -y, mc), mc).round(mathContext);
+		}
+		
 		BigComplex result = BigComplex.ONE;
 		while (y > 0) {
 			if ((y & 1) == 1) {
@@ -138,43 +141,45 @@ public class BigComplexMath {
 		BigDecimal angleTimesN = x.angle(mc).multiply(y, mc);
 		return BigComplex.valueOf(
 				BigDecimalMath.cos(angleTimesN, mc),
-				BigDecimalMath.sin(angleTimesN, mc)).multiply(BigDecimalMath.pow(x.abs(mc), y, mc), mathContext);
+				BigDecimalMath.sin(angleTimesN, mc)).multiply(BigDecimalMath.pow(x.abs(mc), y, mc), mc).round(mathContext);
 	}
 
 	public static BigComplex pow(BigComplex x, BigComplex y, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return exp(y.multiply(log(x, mc), mc), mathContext);
+		return exp(y.multiply(log(x, mc), mc), mc).round(mathContext);
 	}
 	
 	public static BigComplex root_ALTERNATE(BigComplex x, BigDecimal n, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return BigComplex.valueOfPolar(BigDecimalMath.root(x.abs(mc), n, mc), x.angle(mc).divide(n, mc), mathContext);
+		return BigComplex.valueOfPolar(BigDecimalMath.root(x.abs(mc), n, mc), x.angle(mc).divide(n, mc), mc).round(mathContext);
 	}
 	
 	public static BigComplex root(BigComplex x, BigDecimal n, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return pow(x, BigDecimal.ONE.divide(n, mc), mathContext);
+		return pow(x, BigDecimal.ONE.divide(n, mc), mc).round(mathContext);
 	}
 
 	public static BigComplex root(BigComplex x, BigComplex n, MathContext mathContext) {
 		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
 
-		return pow(x, BigComplex.ONE.divide(n, mc), mathContext);
+		return pow(x, BigComplex.ONE.divide(n, mc), mc).round(mathContext);
 	}
 	
 	// TODO add root() for the k'th root - https://math.stackexchange.com/questions/322481/principal-nth-root-of-a-complex-number 
 	
 	private static BigDecimal toRangePi(BigDecimal angle, MathContext mathContext) {
-		BigDecimal pi = BigDecimalMath.pi(mathContext);
+		MathContext mc = new MathContext(mathContext.getPrecision() + 4, mathContext.getRoundingMode());
+
+		BigDecimal pi = BigDecimalMath.pi(mc);
 		
 		if (angle.compareTo(pi.negate()) < 0) {
-			return angle.add(pi, mathContext).add(pi, mathContext);
+			return angle.add(pi, mc).add(pi, mc).round(mathContext);
 		}
 		if (angle.compareTo(pi) > 0) {
-			return angle.subtract(pi, mathContext).subtract(pi, mathContext);
+			return angle.subtract(pi, mc).subtract(pi, mc).round(mathContext);
 		}
 		
 		return angle;
--- ch.obermuhlner.math.big/src/test/java/ch/obermuhlner/math/big/BigComplexMathTest.java
+++ ch.obermuhlner.math.big/src/test/java/ch/obermuhlner/math/big/BigComplexMathTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.util.function.Function;
 
 import org.junit.Test;
 
@@ -38,111 +39,144 @@ public class BigComplexMathTest {
 
 	@Test
 	public void testExp() {
-		assertEquals(
-				BigComplex.valueOf("-1.1312043837568136384312552555107947106288679958265257502", "2.4717266720048189276169308935516645327361903692410081842", MC), 
-				BigComplexMath.exp(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("-1.1312043837568136384312552555107947106288679958265257502", "2.4717266720048189276169308935516645327361903692410081842", MC),
+		   		(mc) -> BigComplexMath.exp(BigComplex.valueOf(1, 2), mc),
+		   	5);
 	}
 
 	@Test
 	public void testSin() {
-		assertEquals(
-				BigComplex.valueOf("3.16577851321616814674073461719190553837911076789146893228", "1.95960104142160589707035204998935827843632016018455965880", MC), 
-				BigComplexMath.sin(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("3.16577851321616814674073461719190553837911076789146893228", "1.95960104142160589707035204998935827843632016018455965880", MC),
+		  		(mc) -> BigComplexMath.sin(BigComplex.valueOf(1, 2), mc),
+		   5);
 	}
 
 	@Test
 	public void testCos() {
-		assertEquals(
-				BigComplex.valueOf("2.03272300701966552943634344849951426373199040663875238194", "-3.05189779915180005751211568689510545288843761773331964466", MC), 
-				BigComplexMath.cos(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("2.03272300701966552943634344849951426373199040663875238194", "-3.05189779915180005751211568689510545288843761773331964466", MC),
+		   		(mc) -> BigComplexMath.cos(BigComplex.valueOf(1, 2), mc),
+		   5);
 	}
 
 	@Test
 	public void testTan() {
-		assertEquals(
-				BigComplex.valueOf("0.03381282607989669028437055972528730164018766933075587436", "1.014793616146633568117054175417967614163217471123533242", MC), 
-				BigComplexMath.tan(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("0.03381282607989669028437055972528730164018766933075587436", "1.014793616146633568117054175417967614163217471123533242", MC),
+		   		(mc) -> BigComplexMath.tan(BigComplex.valueOf(1, 2), mc),
+		   5);
 	}
 
 	@Test
 	public void testLog() {
-		assertEquals(
-				BigComplex.valueOf("0.80471895621705018730037966661309381976280067713425886095", "1.1071487177940905030170654601785370400700476454014326466", MC), 
-				BigComplexMath.log(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("0.80471895621705018730037966661309381976280067713425886095", "1.1071487177940905030170654601785370400700476454014326466", MC),
+		   		(mc) -> BigComplexMath.log(BigComplex.valueOf(1, 2), mc),
+		   5);
 	}
 
 	@Test
 	public void testSqrt() {
-		assertEquals(
-				BigComplex.valueOf("1.2720196495140689642524224617374914917156080418400962486", "0.78615137775742328606955858584295892952312205783772323766", MC), 
-				BigComplexMath.sqrt(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("1.2720196495140689642524224617374914917156080418400962486", "0.78615137775742328606955858584295892952312205783772323766", MC),
+				(mc) -> BigComplexMath.sqrt(BigComplex.valueOf(1, 2), mc),
+		   5);
 	}
 
 	@Test
 	public void testPowInt() {
-		assertEquals(
-				BigComplex.valueOf("18456.81197182559", "-7537.93433754262", MC), 
-				BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), 11, MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("18456.81197182559", "-7537.93433754262", MC),
+		   		(mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), 11, mc),
+		   5);
 
-		assertEquals(
-				BigComplex.valueOf("0.00004643522633206022281216609142474444925257050125807831", "0.00001896457999216310228315702790913198870386323455307511", MC), 
-				BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), -11, MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("0.00004643522633206022281216609142474444925257050125807831", "0.00001896457999216310228315702790913198870386323455307511", MC),
+		   		(mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), -11, mc),
+		   		5);
 	}
 	
 	@Test
 	public void testPow() {
-		assertEquals(
-				BigComplex.valueOf("0.93025840620844374948965939784803335557080436887094399412", "2.5254359132812654282958795629575897052282541443174302618", MC), 
-				BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(1.1), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("0.93025840620844374948965939784803335557080436887094399412", "2.5254359132812654282958795629575897052282541443174302618", MC),
+		   		(mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(1.1), mc),
+		   		5);
 	}
 
 	@Test
 	public void testPowComplex() {
-		assertEquals(
-				BigComplex.valueOf("0.84004892508974359637454837539731545717714689412422564692", "0.25134081792349062667714080016033852435880339768328815847", MC), 
-				BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(0.1, 0.2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("0.84004892508974359637454837539731545717714689412422564692", "0.25134081792349062667714080016033852435880339768328815847", MC),
+		   		(mc) -> BigComplexMath.pow(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(0.1, 0.2), mc),
+		   		5);
 	}
 
 	@Test
 	public void testRoot() {
-		assertEquals(
-				BigComplex.valueOf("1.2589858696615772964532950314498058030426856758251923734", "0.48693816197569980888611771300161883360684382303096712193", MC), 
-				BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(3), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("1.2589858696615772964532950314498058030426856758251923734", "0.48693816197569980888611771300161883360684382303096712193", MC),
+		   		(mc) -> BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigDecimal.valueOf(3), mc),
+		   		5);
 	}
 
 	@Test
 	public void testRootComplex() {
-		assertEquals(
-				BigComplex.valueOf("1.4494235461237517229913668192323085954480223365111721042", "0.17040446483770403353362509072059809846680452097000085527", MC), 
-				BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(3, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("1.4494235461237517229913668192323085954480223365111721042", "0.17040446483770403353362509072059809846680452097000085527", MC),
+		   		(mc) -> BigComplexMath.root(BigComplex.valueOf(1.1, 2.2), BigComplex.valueOf(3, 2), mc),
+		   		5);
 	}
 
 	@Test
 	public void testAsin() {
-		assertEquals(
-				BigComplex.valueOf("0.42707858639247612548064688331895685930333615088099242008", "1.5285709194809981612724561847936733932886832962358541247", MC), 
-				BigComplexMath.asin(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("0.42707858639247612548064688331895685930333615088099242008", "1.5285709194809981612724561847936733932886832962358541247", MC),
+		   		(mc) -> BigComplexMath.asin(BigComplex.valueOf(1, 2), mc),
+		   		5);
 	}
 
 	@Test
 	public void testAcos() {
-		assertEquals(
-				BigComplex.valueOf("1.14371774040242049375067480832079458279524854880656049040", "-1.52857091948099816127245618479367339328868329623585412477", MC), 
-				BigComplexMath.acos(BigComplex.valueOf(1, 2), MC));
+		assertPrecisionCalculation(
+				BigComplex.valueOf("1.14371774040242049375067480832079458279524854880656049040", "-1.52857091948099816127245618479367339328868329623585412477", MC),
+		   		(mc) -> BigComplexMath.acos(BigComplex.valueOf(1, 2), mc),
+		   		5);
 	}
 
 	@Test
 	public void testAtan() {
-		assertEquals(
+		assertPrecisionCalculation(
 				BigComplex.valueOf("1.3389725222944935611241935759091442410843161725444927785", "0.40235947810852509365018983330654690988140033856712943047", MC), 
-				BigComplexMath.atan(BigComplex.valueOf(1, 2), MC));
+		   		(mc) -> BigComplexMath.atan(BigComplex.valueOf(1, 2), mc),
+		   5);
 	}
 
 	@Test
 	public void testAcot() {
-		assertEquals(
+		assertPrecisionCalculation(
 				BigComplex.valueOf("0.23182380450040305810712811573060720101426852714306013190", "-0.40235947810852509365018983330654690988140033856712943047", MC), 
-				BigComplexMath.acot(BigComplex.valueOf(1, 2), MC));
+		   		(mc) -> BigComplexMath.acot(BigComplex.valueOf(1, 2), mc),
+		   		5);
+	}
+
+	private void assertPrecisionCalculation(BigComplex expected, Function<MathContext, BigComplex> precisionCalculation, int startPrecision) {
+		assertPrecisionCalculation(expected, precisionCalculation, startPrecision, expected.re.precision());
+	}
+
+	private void assertPrecisionCalculation(BigComplex expected, Function<MathContext, BigComplex> precisionCalculation, int startPrecision, int endPrecision) {
+		int precision = startPrecision;
+		while (precision <= endPrecision) {
+			MathContext mathContext = new MathContext(precision);
+			System.out.println("precision=" + precision + " " + expected.round(mathContext));
+			assertEquals(
+			   "precision=" + precision,
+			   expected.round(mathContext).toString(),
+			   precisionCalculation.apply(mathContext).toString());
+			precision += 5;
+		}
 	}
 
 }
