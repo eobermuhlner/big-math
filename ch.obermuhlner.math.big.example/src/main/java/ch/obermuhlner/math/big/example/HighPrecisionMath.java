6cbd44199d0347b4e37beb87546bbb7dba2a618c
Eric Obermühlner <eobermuhlner@gmail.com>
2018-03-30 17:47:00 +0200

    make constants final

--- ch.obermuhlner.math.big.example/src/main/java/ch/obermuhlner/math/big/example/HighPrecisionMath.java
+++ ch.obermuhlner.math.big.example/src/main/java/ch/obermuhlner/math/big/example/HighPrecisionMath.java
@@ -9,9 +9,9 @@ import java.math.MathContext;
  * Example class for a fictive project that needs a specific precision in its calculations.
  */
 public class HighPrecisionMath {
-	public static MathContext MATH_CONTEXT = new MathContext(100);
+	public static final MathContext MATH_CONTEXT = new MathContext(100);
 
-	public static BigDecimal PI = BigDecimalMath.pi(MATH_CONTEXT);
+	public static final BigDecimal PI = BigDecimalMath.pi(MATH_CONTEXT);
 
 	public static BigDecimal pow(BigDecimal x, BigDecimal y) {
 		return BigDecimalMath.pow(x, y, MATH_CONTEXT);
