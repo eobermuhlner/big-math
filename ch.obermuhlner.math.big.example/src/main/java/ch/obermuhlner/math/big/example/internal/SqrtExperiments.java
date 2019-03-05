579847457727350efeab7d3d99520487e49e371e
Eric Obermühlner <eobermuhlner@gmail.com>
2019-03-05 11:51:53 +0100

    comment out java9 sqrt() function calls

--- ch.obermuhlner.math.big.example/src/main/java/ch/obermuhlner/math/big/example/internal/SqrtExperiments.java
+++ ch.obermuhlner.math.big.example/src/main/java/ch/obermuhlner/math/big/example/internal/SqrtExperiments.java
@@ -15,10 +15,12 @@ public class SqrtExperiments {
         BigDecimal v = new BigDecimal("1.00000000000000000000001");
         BigDecimal v2 = v.multiply(v);
 
+        /*
         System.out.println("java9    " + BigDecimal.valueOf(2).sqrt(mc));
         System.out.println("java9    " + BigDecimal.valueOf(4).sqrt(mc));
         System.out.println("java9    " + BigDecimal.valueOf(0.01).sqrt(mc));
         System.out.println("java9    " + v2.sqrt(mc));
+        */
 
         System.out.println("big-math " + BigDecimalMath.sqrt(BigDecimal.valueOf(2), mc));
         System.out.println("big-math " + BigDecimalMath.sqrt(BigDecimal.valueOf(4), mc));
