06f3ccec38a8a9a3f1e7b46bd3c436a530e97f1c
Eric Obermühlner <eobermuhlner@gmail.com>
2019-04-04 21:53:41 +0200

    add unit tests for skewness/kurtosis collectors

--- ch.obermuhlner.math.big/src/test/java/ch/obermuhlner/math/big/statistics/StatisticsCollectorsTest.java
+++ ch.obermuhlner.math.big/src/test/java/ch/obermuhlner/math/big/statistics/StatisticsCollectorsTest.java
@@ -69,6 +69,51 @@ public class StatisticsCollectorsTest {
     }
 
     @Test
+    public void testPopulationSkewness() {
+        // https://www.wolframalpha.com/input/?i=Skewness%5B1,2,3,4,4%5D
+        BigDecimal result = Stream.of(1, 2, 3, 4, 4)
+                .map(x -> BigDecimal.valueOf(x))
+                .collect(StatisticsCollectors.populationSkewness(MC)).round(new MathContext(5));
+        assertBigDecimal(BigDecimal.valueOf(-0.36317), result);
+    }
+
+    @Test
+    public void testPopulationKurtosis() {
+        // https://www.wolframalpha.com/input/?i=kurtosis+1,2,3,4,4
+        BigDecimal result = Stream.of(1, 2, 3, 4, 4)
+                .map(x -> BigDecimal.valueOf(x))
+                .collect(StatisticsCollectors.populationKurtosis(MC)).round(new MathContext(5));
+        assertBigDecimal(BigDecimal.valueOf(1.6280), result);
+    }
+
+    @Test
+    public void testPopulationExcessKurtosis() {
+        // https://www.wolframalpha.com/input/?i=kurtosis+1,2,3,4,4
+        BigDecimal result = Stream.of(1, 2, 3, 4, 4)
+                .map(x -> BigDecimal.valueOf(x))
+                .collect(StatisticsCollectors.populationExcessKurtosis(MC)).round(new MathContext(5));
+        assertBigDecimal(BigDecimal.valueOf(1.6280 - 3), result);
+    }
+
+    @Test
+    public void testSampleSkewness() {
+        // Excel: =SKEW(1;2;3;4;4)
+        BigDecimal result = Stream.of(1, 2, 3, 4, 4)
+                .map(x -> BigDecimal.valueOf(x))
+                .collect(StatisticsCollectors.sampleSkewness(MC)).round(new MathContext(5));
+        assertBigDecimal(BigDecimal.valueOf(-0.54139), result);
+    }
+
+    @Test
+    public void testSampleExcessKurtosis() {
+        // Excel: =KURT(1;2;3;4;4)
+        BigDecimal result = Stream.of(1, 2, 3, 4, 4)
+                .map(x -> BigDecimal.valueOf(x))
+                .collect(StatisticsCollectors.sampleExcessKurtosis(MC)).round(new MathContext(5));
+        assertBigDecimal(BigDecimal.valueOf(-1.4879), result);
+    }
+
+    @Test
     public void testHistogram() {
         Histogram histogram = Stream.of(1, 2, 3, 5, 5, 5, 5, 6, 6, 7, 8, 8, 9)
                 .parallel()
