3a86c1c3e6b9c604e4516b49afad7156cea0d007
Eric Obermühlner <eobermuhlner@gmail.com>
2019-10-07 16:00:18 +0200

    write performance regression result to performance.csv

--- ch.obermuhlner.math.big.example/src/main/java/ch/obermuhlner/math/big/example/internal/PerformanceRegressionBigDecimalMath.java
+++ ch.obermuhlner.math.big.example/src/main/java/ch/obermuhlner/math/big/example/internal/PerformanceRegressionBigDecimalMath.java
@@ -1,5 +1,6 @@
 package ch.obermuhlner.math.big.example.internal;
 
+import java.io.*;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.ArrayList;
@@ -29,87 +30,98 @@ public class PerformanceRegressionBigDecimalMath {
 	private static final BigDecimal value100 = BigDecimal.valueOf(100);
 
 	public static void main(String[] args) {
-		performanceRegression(300);
+		try {
+			PrintStream out = new PrintStream(new FileOutputStream("performance.csv"));
+			performanceRegression(out, 300);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private static void performanceRegression(int precision) {
+	private static void performanceRegression(PrintStream out, int precision) {
 		MathContext mathContext = new MathContext(precision);
+
+		out.printf("%-20s, %15s, %15s, %15s, %15s\n", "Name", "Average [ns]", "Median [ns]", "Min [ns]", "Max [ns]");
 		
-		System.out.printf("%-20s, %15s, %15s, %15s, %15s\n", "Name", "Average [ns]", "Median [ns]", "Min [ns]", "Max [ns]");
+		measurePerformance(out, "log(0.1)", mathContext, () -> BigDecimalMath.log(value0_1, mathContext));
+		measurePerformance(out, "log(0.2)", mathContext, () -> BigDecimalMath.log(value0_2, mathContext));
+		measurePerformance(out, "log(0.3)", mathContext, () -> BigDecimalMath.log(value0_3, mathContext));
+		measurePerformance(out, "log(0.4)", mathContext, () -> BigDecimalMath.log(value0_4, mathContext));
+		measurePerformance(out, "log(0.5)", mathContext, () -> BigDecimalMath.log(value0_5, mathContext));
+		measurePerformance(out, "log(0.6)", mathContext, () -> BigDecimalMath.log(value0_6, mathContext));
+		measurePerformance(out, "log(0.7)", mathContext, () -> BigDecimalMath.log(value0_7, mathContext));
+		measurePerformance(out, "log(0.8)", mathContext, () -> BigDecimalMath.log(value0_8, mathContext));
+		measurePerformance(out, "log(0.9)", mathContext, () -> BigDecimalMath.log(value0_9, mathContext));
+		measurePerformance(out, "log(1)", mathContext, () -> BigDecimalMath.log(value1, mathContext));
+		measurePerformance(out, "log(2)", mathContext, () -> BigDecimalMath.log(value2, mathContext));
+		measurePerformance(out, "log(10)", mathContext, () -> BigDecimalMath.log(value10, mathContext));
+		measurePerformance(out, "log(100)", mathContext, () -> BigDecimalMath.log(value100, mathContext));
+		measurePerformance(out, "log2(2)", mathContext, () -> BigDecimalMath.log2(value2, mathContext));
+		measurePerformance(out, "log10(2)", mathContext, () -> BigDecimalMath.log10(value2, mathContext));
+		measurePerformance(out, "exp(2)", mathContext, () -> BigDecimalMath.exp(value2, mathContext));
+		measurePerformance(out, "sqrt(2)", mathContext, () -> BigDecimalMath.sqrt(value2, mathContext));
+		measurePerformance(out, "sqrt(9)", mathContext, () -> BigDecimalMath.sqrt(value9, mathContext));
+		measurePerformance(out, "pow(2;3)", mathContext, () -> BigDecimalMath.pow(value2, value3, mathContext));
+		measurePerformance(out, "pow(2;0.1)", mathContext, () -> BigDecimalMath.pow(value2, value0_1, mathContext));
+		measurePerformance(out, "root(2;3)", mathContext, () -> BigDecimalMath.root(value2, value3, mathContext));
+		measurePerformance(out, "sin(2)", mathContext, () -> BigDecimalMath.sin(value2, mathContext));
+		measurePerformance(out, "sin(100)", mathContext, () -> BigDecimalMath.sin(value100, mathContext));
+		measurePerformance(out, "cos(2)", mathContext, () -> BigDecimalMath.cos(value2, mathContext));
+		measurePerformance(out, "cos(100)", mathContext, () -> BigDecimalMath.cos(value100, mathContext));
+		measurePerformance(out, "tan(2)", mathContext, () -> BigDecimalMath.tan(value2, mathContext));
+		measurePerformance(out, "cot(2)", mathContext, () -> BigDecimalMath.cot(value2, mathContext));
+		measurePerformance(out, "asin(0.1)", mathContext, () -> BigDecimalMath.asin(value0_1, mathContext));
+		measurePerformance(out, "asin(0.8)", mathContext, () -> BigDecimalMath.asin(value0_8, mathContext));
+		measurePerformance(out, "acos(0.1)", mathContext, () -> BigDecimalMath.acos(value0_1, mathContext));
+		measurePerformance(out, "atan(0.1)", mathContext, () -> BigDecimalMath.atan(value0_1, mathContext));
+		measurePerformance(out, "acot(0.1)", mathContext, () -> BigDecimalMath.acot(value0_1, mathContext));
+		measurePerformance(out, "sinh(2)", mathContext, () -> BigDecimalMath.sinh(value2, mathContext));
+		measurePerformance(out, "cosh(2)", mathContext, () -> BigDecimalMath.cosh(value2, mathContext));
+		measurePerformance(out, "tanh(2)", mathContext, () -> BigDecimalMath.tanh(value2, mathContext));
+		measurePerformance(out, "asinh(0.1)", mathContext, () -> BigDecimalMath.asinh(value0_1, mathContext));
+		measurePerformance(out, "acosh(2)", mathContext, () -> BigDecimalMath.acosh(value2, mathContext));
+		measurePerformance(out, "atanh(0.1)", mathContext, () -> BigDecimalMath.atanh(value0_1, mathContext));
+		measurePerformance(out, "acoth(2)", mathContext, () -> BigDecimalMath.acoth(value2, mathContext));
+		measurePerformance(out, "coth(2)", mathContext, () -> BigDecimalMath.coth(value2, mathContext));
 		
-		measurePerformance("log(0.1)", mathContext, () -> BigDecimalMath.log(value0_1, mathContext));
-		measurePerformance("log(0.2)", mathContext, () -> BigDecimalMath.log(value0_2, mathContext));
-		measurePerformance("log(0.3)", mathContext, () -> BigDecimalMath.log(value0_3, mathContext));
-		measurePerformance("log(0.4)", mathContext, () -> BigDecimalMath.log(value0_4, mathContext));
-		measurePerformance("log(0.5)", mathContext, () -> BigDecimalMath.log(value0_5, mathContext));
-		measurePerformance("log(0.6)", mathContext, () -> BigDecimalMath.log(value0_6, mathContext));
-		measurePerformance("log(0.7)", mathContext, () -> BigDecimalMath.log(value0_7, mathContext));
-		measurePerformance("log(0.8)", mathContext, () -> BigDecimalMath.log(value0_8, mathContext));
-		measurePerformance("log(0.9)", mathContext, () -> BigDecimalMath.log(value0_9, mathContext));
-		measurePerformance("log(1)", mathContext, () -> BigDecimalMath.log(value1, mathContext));
-		measurePerformance("log(2)", mathContext, () -> BigDecimalMath.log(value2, mathContext));
-		measurePerformance("log(10)", mathContext, () -> BigDecimalMath.log(value10, mathContext));
-		measurePerformance("log(100)", mathContext, () -> BigDecimalMath.log(value100, mathContext));
-		measurePerformance("log2(2)", mathContext, () -> BigDecimalMath.log2(value2, mathContext));
-		measurePerformance("log10(2)", mathContext, () -> BigDecimalMath.log10(value2, mathContext));
-		measurePerformance("exp(2)", mathContext, () -> BigDecimalMath.exp(value2, mathContext));
-		measurePerformance("sqrt(2)", mathContext, () -> BigDecimalMath.sqrt(value2, mathContext));
-		measurePerformance("sqrt(9)", mathContext, () -> BigDecimalMath.sqrt(value9, mathContext));
-		measurePerformance("pow(2;3)", mathContext, () -> BigDecimalMath.pow(value2, value3, mathContext));
-		measurePerformance("pow(2;0.1)", mathContext, () -> BigDecimalMath.pow(value2, value0_1, mathContext));
-		measurePerformance("root(2;3)", mathContext, () -> BigDecimalMath.root(value2, value3, mathContext));
-		measurePerformance("sin(2)", mathContext, () -> BigDecimalMath.sin(value2, mathContext));
-		measurePerformance("sin(100)", mathContext, () -> BigDecimalMath.sin(value100, mathContext));
-		measurePerformance("cos(2)", mathContext, () -> BigDecimalMath.cos(value2, mathContext));
-		measurePerformance("cos(100)", mathContext, () -> BigDecimalMath.cos(value100, mathContext));
-		measurePerformance("tan(2)", mathContext, () -> BigDecimalMath.tan(value2, mathContext));
-		measurePerformance("cot(2)", mathContext, () -> BigDecimalMath.cot(value2, mathContext));
-		measurePerformance("asin(0.1)", mathContext, () -> BigDecimalMath.asin(value0_1, mathContext));
-		measurePerformance("asin(0.8)", mathContext, () -> BigDecimalMath.asin(value0_8, mathContext));
-		measurePerformance("acos(0.1)", mathContext, () -> BigDecimalMath.acos(value0_1, mathContext));
-		measurePerformance("atan(0.1)", mathContext, () -> BigDecimalMath.atan(value0_1, mathContext));
-		measurePerformance("acot(0.1)", mathContext, () -> BigDecimalMath.acot(value0_1, mathContext));
-		measurePerformance("sinh(2)", mathContext, () -> BigDecimalMath.sinh(value2, mathContext));
-		measurePerformance("cosh(2)", mathContext, () -> BigDecimalMath.cosh(value2, mathContext));
-		measurePerformance("tanh(2)", mathContext, () -> BigDecimalMath.tanh(value2, mathContext));
-		measurePerformance("asinh(0.1)", mathContext, () -> BigDecimalMath.asinh(value0_1, mathContext));
-		measurePerformance("acosh(2)", mathContext, () -> BigDecimalMath.acosh(value2, mathContext));
-		measurePerformance("atanh(0.1)", mathContext, () -> BigDecimalMath.atanh(value0_1, mathContext));
-		measurePerformance("acoth(2)", mathContext, () -> BigDecimalMath.acoth(value2, mathContext));
-		measurePerformance("coth(2)", mathContext, () -> BigDecimalMath.coth(value2, mathContext));
-		
-		measurePerformance("BigFloat.add", mathContext, () -> BigFloat.context(mathContext).valueOf(123).add(456).toBigDecimal());
-		measurePerformance("BigFloat.subtract", mathContext, () -> BigFloat.context(mathContext).valueOf(123).subtract(456).toBigDecimal());
-		measurePerformance("BigFloat.multiply", mathContext, () -> BigFloat.context(mathContext).valueOf(123).multiply(456).toBigDecimal());
-		measurePerformance("BigFloat.divide", mathContext, () -> BigFloat.context(mathContext).valueOf(123).divide(456).toBigDecimal());
+		measurePerformance(out, "BigFloat.add", mathContext, () -> BigFloat.context(mathContext).valueOf(123).add(456).toBigDecimal());
+		measurePerformance(out, "BigFloat.subtract", mathContext, () -> BigFloat.context(mathContext).valueOf(123).subtract(456).toBigDecimal());
+		measurePerformance(out, "BigFloat.multiply", mathContext, () -> BigFloat.context(mathContext).valueOf(123).multiply(456).toBigDecimal());
+		measurePerformance(out, "BigFloat.divide", mathContext, () -> BigFloat.context(mathContext).valueOf(123).divide(456).toBigDecimal());
 	}
 
-	private static void measurePerformance(String name, MathContext mathContext, Runnable function) {
-		int warmup = 1000;
-		for (int i = 0; i < warmup; i++) {
-			function.run();
-		}
+	private static void measurePerformance(PrintStream out, String name, MathContext mathContext, Runnable function) {
+		try {
+			int warmup = 1000;
+			for (int i = 0; i < warmup; i++) {
+				function.run();
+			}
 
-		int count = 1000;
-		long totalNanos = 0;
-		long minNanos = Long.MAX_VALUE;
-		long maxNanos = Long.MIN_VALUE;
-		List<Long> allNanos = new ArrayList<>();
-		
-		for (int i = 0; i < count; i++) {
-			StopWatch stopWatch = new StopWatch();
-			function.run();
-			long nanos = stopWatch.getElapsedNanos();
-			
-			totalNanos += nanos;
-			minNanos = Math.min(minNanos, nanos);
-			maxNanos = Math.max(maxNanos, nanos);
-			allNanos.add(nanos);
+			int count = 1000;
+			long totalNanos = 0;
+			long minNanos = Long.MAX_VALUE;
+			long maxNanos = Long.MIN_VALUE;
+			List<Long> allNanos = new ArrayList<>();
+
+			for (int i = 0; i < count; i++) {
+				StopWatch stopWatch = new StopWatch();
+				function.run();
+				long nanos = stopWatch.getElapsedNanos();
+
+				totalNanos += nanos;
+				minNanos = Math.min(minNanos, nanos);
+				maxNanos = Math.max(maxNanos, nanos);
+				allNanos.add(nanos);
+			}
+
+			long avgNanos = totalNanos / count;
+			long medianNanos = median(allNanos);
+			System.out.printf("%-20s: %15d\n", name, medianNanos);
+
+			out.printf("%-20s, %15d, %15d, %15d, %15d\n", name, avgNanos, medianNanos, minNanos, maxNanos);
+		} catch (Throwable e) {
+			out.printf("%-20s, %15s, %15s, %15s, %15s\n", name, "", "", "", "");
 		}
-		
-		long avgNanos = totalNanos / count;
-		long medianNanos = median(allNanos);
-		System.out.printf("%-20s, %15d, %15d, %15d, %15d\n", name, avgNanos, medianNanos, minNanos, maxNanos);
 	}
 
 	private static long median(List<Long> values) {
