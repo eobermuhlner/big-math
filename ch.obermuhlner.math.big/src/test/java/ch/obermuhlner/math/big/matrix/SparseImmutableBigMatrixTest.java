e41ee8f29219631beec7a559b420b904a2971687
Eric Obermuehlner <eric.obermuehlner@assetmax.ch>
2019-11-14 11:29:32 +0100

    test sparse sum()

--- ch.obermuhlner.math.big/src/test/java/ch/obermuhlner/math/big/matrix/SparseImmutableBigMatrixTest.java
+++ ch.obermuhlner.math.big/src/test/java/ch/obermuhlner/math/big/matrix/SparseImmutableBigMatrixTest.java
@@ -1,9 +1,23 @@
 package ch.obermuhlner.math.big.matrix;
 
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static ch.obermuhlner.util.AssertUtil.assertBigDecimal;
+import static java.math.BigDecimal.valueOf;
+
 public class SparseImmutableBigMatrixTest extends AbstractImmutableBigMatrixTest {
 
     @Override
     protected ImmutableBigMatrix createImmutableBigMatrix(int rows, int columns, double... values) {
         return ImmutableBigMatrix.sparseMatrix(rows, columns, values);
+    }
+
+    @Test(timeout = 10)
+    public void testSparseSum() {
+        ImmutableBigMatrix m = createImmutableBigMatrix(10000, 20000, 1, 2, 3);
+
+        assertBigDecimal(valueOf(1+2+3), m.sum());
     }
 }
