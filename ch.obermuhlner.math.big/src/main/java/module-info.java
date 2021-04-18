edcdb90176812ef7d97ed4748234878a6fdcf710
Eric Obermühlner <eobermuhlner@gmail.com>
2021-04-18 14:47:54 +0200

    remove module-info to support Java 8

--- ch.obermuhlner.math.big/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ch.obermuhlner.math.big {
-    
-    /**
-     * Exports.
-     */
-    exports ch.obermuhlner.math.big;
-    exports ch.obermuhlner.math.big.stream;
-    exports ch.obermuhlner.math.big.internal;
-}
